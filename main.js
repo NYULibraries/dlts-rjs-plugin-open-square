83c0f85e340c5e17aaf68a3205898a2bf720449a
David <da70@nyu.edu>
2015-09-23 16:18:49 -0400

    main.js: initial commit of shell

---
+++ main.js
@@ -0,0 +1,9 @@
+define( [ 'readium_js_plugins', 'jquery' ], function ( Plugins, $ ) {
+
+    Plugins.register( 'dltsRjsPluginOaBooks', function ( api ) {
+
+        api.plugin.warn( 'Registered dltsRjsPluginOaBooks' );
+
+    } );
+
+} );
