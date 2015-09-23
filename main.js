021b968884bf8eeba2340651481ac08c4ad4c9d7
David <da70@nyu.edu>
2015-09-23 17:28:55 -0400

    Hide Readium log (about button

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ define( [ 'readium_js_plugins', 'jquery' ], function ( Plugins, $ ) {
 
     Plugins.register( 'dltsRjsPluginOaBooks', function ( api ) {
 
-        api.plugin.warn( 'Registered dltsRjsPluginOaBooks' );
+        var $readiumAboutButton = $( '#aboutButt1' );
+
+        // Remove Readium logo
+        // There are many buttons default .navbar-left that have visibility: hidden
+        // Following the same pattern.
+        // TODO: figure out if can eliminate the flicker.  Not sure if we can
+        // because we can't force this to be called any earlier.
+        $readiumAboutButton.css( { visibility : 'hidden' } );
 
     } );
 
