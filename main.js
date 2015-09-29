e25dc7568a8b91c36cf271d37c2bc8cb4e28f8a5
David <da70@nyu.edu>
2015-09-29 14:05:08 -0400

    Extract function hideReadiumAboutButton and call from new function doCustomizations

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ define( [ 'readium_js_plugins', 'jquery' ], function ( Plugins, $ ) {
 
     Plugins.register( 'dltsRjsPluginOaBooks', function ( api ) {
 
+        doCustomizations();
+
+    } );
+
+    function doCustomizations() {
+        hideReadiumAboutButton();
+    }
+
+    function hideReadiumAboutButton() {
         var $readiumAboutButton = $( '#aboutButt1' );
 
         // Remove Readium logo
@@ -10,7 +19,7 @@ define( [ 'readium_js_plugins', 'jquery' ], function ( Plugins, $ ) {
         // TODO: figure out if can eliminate the flicker.  Not sure if we can
         // because we can't force this to be called any earlier.
         $readiumAboutButton.css( { visibility : 'hidden' } );
-
-    } );
+    }
 
 } );
+
