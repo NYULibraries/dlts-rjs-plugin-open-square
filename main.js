56490534965bedf5f690f71f93b58ee3baf3f239
David <da70@nyu.edu>
2015-10-01 17:01:07 -0400

    Final implementation of hideNavbarUnlessHoverOver

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ define( [ 'readium_js_plugins', 'jquery', './DltsEpubReader' ], function ( Plugi
             api.reader.on(
                 ReadiumSDK.Events.CONTENT_DOCUMENT_LOADED,
                 function ( $iframe, spineItem ) {
-                    hideNavbarUnlessHoverOver( api, $iframe, DltsEpubReader );
+                    hideNavbarUnlessHoverOver( $iframe, DltsEpubReader );
                 }
             );
         }
@@ -46,22 +46,40 @@ define( [ 'readium_js_plugins', 'jquery', './DltsEpubReader' ], function ( Plugi
 
     }
 
-    function hideNavbarUnlessHoverOver( api, $iframe, DltsEpubReader ) {
-        var reader         = api.reader,
-            $window        = $( window ),
-            $contentWindow = $( $iframe[ 0 ].contentWindow );
+    function hideNavbarUnlessHoverOver( $iframe, DltsEpubReader ) {
+        var $window        = $( window ),
+            $iframeContentWindow = $( $iframe[ 0 ].contentWindow );
 
+        // First, we remove existing mousemove event handlers that were added
+        // by EpubReader
         $window.off( 'mousemove' );
         // No API call on EpubReader to remove this listener, though there is
-        // an API for adding on, which we use later.
-        $contentWindow.off( 'mousemove' );
+        // an API for adding one (addIFrameEventListener).
+        $iframeContentWindow.off( 'mousemove' );
 
+        // This call is similar to what EpubReader.installReaderEventHandlers
+        // uses to add mousemove event handler to window.  The crucial difference
+        // is that we pass in true for the `immediate` param, so that hideLoop
+        // knows not to show the UI for 4 seconds before hiding goes into effect.
         $window.on( 'mousemove', function() {
             DltsEpubReader.hideLoop( null, true );
         });
-        reader.addIFrameEventListener('mousemove', function() {
-            DltsEpubReader.hideLoop( null, true );
-        });
+
+        // The commented-out code below mirrors EpubReader.initReadium's adding
+        // of event handling to the iframe.  For some reason the handler never
+        // runs, it's always the above $window event capture that fires.  Tested
+        // the core code and it is the case that even if their addIFrameEventListener
+        // call is disabled, their `$window.on( 'mousemove'...` still takes care of
+        // everything.  Ideally we'd try to mirror this call for good measure,
+        // but I don't think it's worth it at the moment.  There are probably
+        // event bubbling and capturing rules involved and those vary by browser.
+        // We already capture everything, so leave this go for now.
+        // Keep this in comments though so that we remember that this has already
+        // been worked through.
+        //
+        //reader.addIFrameEventListener('mousemove', function() {
+        //    DltsEpubReader.hideLoop( null, true );
+        //});
     }
 
 } );
