9e1440c52b4c9df431c9b394b97fcff8f3f243be
David <da70@nyu.edu>
2015-10-02 15:57:41 -0400

    Add documenting comments to EpubReaderDoppelganger.js

--- EpubReaderDoppelganger.js
+++ EpubReaderDoppelganger.js
@@ -1,3 +1,19 @@
+/**
+ * @file Doppelganger of readium-js-viewer/src/js/EpubReader.js, for customization.
+ */
+
+// This is a modified copy of Readium's EpubReader module, whose methods we need
+// to override or modify.  EpubReader is not available to our plugin code so
+// we need our own "doppelganger" to stand in and provide similar intentions but
+// modified behavior.
+
+// For now, when copying over a method from the original file, we keep the code
+// as close to the original as possible, including comment typos, coding style
+// and formatting inconsistent with ours, etc.  In theory this might make it
+// easier to do future modifications when Readium code updates and breaks our
+// plugin.  The code similarity might not be worth it in the end, though.  We'll
+// see.
+
 define( [ 'jquery' ], function ( $ ) {
 
     DltsEpubReader = {
@@ -5,6 +21,8 @@ define( [ 'jquery' ], function ( $ ) {
         hideTimeoutId : undefined
     };
 
+    // Copied from readium-js-viewer/src/js/EpubReader.js hideUI(), with
+    // slight modifications.
     DltsEpubReader.hideUI = function(){
         this.hideTimeoutId = null;
         // don't hide it toolbar while toc open in non-embedded mode
@@ -28,6 +46,8 @@ define( [ 'jquery' ], function ( $ ) {
         $(document.body).addClass('hide-ui');
     }
 
+    // Copied from readium-js-viewer/src/js/EpubReader.js hideLoop(), with
+    // slight modifications.
     DltsEpubReader.hideLoop = function(e, immediate){
 
         // if (!embedded){
