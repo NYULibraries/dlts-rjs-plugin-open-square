97d8bff2724b717486251b0279ec5c33ca8886a5
David <da70@nyu.edu>
2015-10-23 17:46:40 -0400

    Improve comment for Util.isTouchDevice()

--- Util.js
+++ Util.js
@@ -7,12 +7,19 @@ define(
 
         var Util = {};
 
-        // We might not be taking into account IE:
-        // http://ctrlq.org/code/19616-detect-touch-screen-javascript
-        // But at the moment we don't consider IE a priority.
         Util.isTouchDevice = function() {
             // From Modernizr
             // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/touchevents.js
+
+            // Note that besides the detection below Modernizer does another test
+            // that involves attempting to inject an element with certain style
+            // rules and checking to see if it succeeds.  We are not using that
+            // part and at the moment are avoiding dragging Modernizr in as a
+            // dependency for this because there might already be a timing issue
+            // with flickering elements.
+
+            // Note that our minimal test probably won't work for IE,
+            // but at the moment we don't consider IE a priority.
             return (
                        'ontouchstart' in window ||
                        ( window.DocumentTouch && document instanceof DocumentTouch )
